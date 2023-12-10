f8319fb6bcbc7dde83c8bdae157832f41f69d15f
PedroCNeto <pedrocastelarineto@gmail.com>
2023-12-10 10:48:18 -0300

    correcao das jogadas

---
+++ scriptVelha.js
@@ -0,0 +1,78 @@
+var tituloVencedor = document.getElementById('tituloVencedor');
+var board = document.getElementById('board');
+var titulo = document.getElementById('titulo');
+var quadrados = [];
+var matriz = ['', '', '', '', '', '', '', '', ''];
+var jogadas = 0;
+var player = 1;
+function checaGanhador() {
+    var winningCombos = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
+        [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
+        [0, 4, 8], [2, 4, 6] // Diagonals
+    ];
+    return winningCombos.some(function (combo) {
+        var a = combo[0], b = combo[1], c = combo[2];
+        return (matriz[a] !== '' && matriz[a] === matriz[b] && matriz[a] === matriz[c]);
+    });
+}
+function empate() {
+    matriz = ['', '', '', '', '', '', '', '', ''];
+    tituloVencedor.innerHTML = "Empate";
+    board.classList.add('blur');
+}
+function limparTabuleiro() {
+    quadrados.forEach(function (element) {
+        element.style.backgroundImage = "";
+    });
+    matriz = ['', '', '', '', '', '', '', '', ''];
+    board.classList.remove('blur');
+    tituloVencedor.innerHTML = '';
+}
+function colocarPeca(i, square) {
+    if (player == 1 && !(matriz[i] === 'x' || matriz[i] === 'o')) {
+        square.style.backgroundImage = "url('../imgs/o.png')";
+        square.style.backgroundSize = "100% 100%";
+        player = 0;
+        matriz[i] = 'o';
+        jogadas++;
+        if (checaGanhador() === true) {
+            matriz = ['', '', '', '', '', '', '', '', ''];
+            tituloVencedor.innerHTML = "O Circulo Ganhou!";
+            board.classList.add('blur');
+            jogadas = 0;
+        }
+        else if (checaGanhador() === false && jogadas === 9) {
+            empate();
+            jogadas = 0;
+        }
+    }
+    else if (player == 0 && !(matriz[i] === 'x' || matriz[i] === 'o')) {
+        square.style.backgroundImage = "url('../imgs/x.png')";
+        square.style.backgroundSize = "100% 100%";
+        player = 1;
+        matriz[i] = 'x';
+        jogadas++;
+        if (checaGanhador() === true) {
+            matriz = ['', '', '', '', '', '', '', '', ''];
+            tituloVencedor.innerHTML = "O X Ganhou!";
+            board.classList.add('blur');
+            jogadas = 0;
+        }
+        else if (checaGanhador() === false && jogadas === 9) {
+            empate();
+            jogadas = 0;
+        }
+    }
+}
+var _loop_1 = function (i) {
+    var square = document.createElement('div');
+    square.className = 'square';
+    square.id = "square".concat(i);
+    square.addEventListener('click', function () { return colocarPeca(i, square); });
+    board.appendChild(square);
+    quadrados.push(square);
+};
+for (var i = 0; i < 9; i++) {
+    _loop_1(i);
+}
---
+++ scriptVelha.ts
@@ -0,0 +1,87 @@
+
+const tituloVencedor = document.getElementById('tituloVencedor') as HTMLTitleElement;
+const board = document.getElementById('board') as HTMLTableCellElement;
+const titulo = document.getElementById('titulo') as HTMLTitleElement;
+const quadrados : HTMLElement[] = [];
+
+var matriz : string[] = ['', '', '', '', '', '', '', '', ''];
+var jogadas : number = 0;
+var player : number = 1;
+function checaGanhador(){
+    const winningCombos = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
+        [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
+        [0, 4, 8], [2, 4, 6]             // Diagonals
+    ];
+
+    return winningCombos.some(combo => {
+
+        const [a, b, c] : number[] = combo;
+        return (matriz[a] !== '' && matriz[a] === matriz[b] && matriz[a] === matriz[c]);
+    });
+}
+
+function empate(){
+    matriz = ['', '', '', '', '', '', '', '', ''];
+    tituloVencedor.innerHTML = "Empate"
+    board.classList.add('blur');
+}
+
+
+function limparTabuleiro(){
+    quadrados.forEach(element => {
+        element.style.backgroundImage = "";
+    });
+    matriz = ['', '', '', '', '', '', '', '', ''];
+    board.classList.remove('blur');
+    tituloVencedor.innerHTML = '';
+}
+
+
+function colocarPeca(i : number,square : HTMLDivElement){
+    if(player == 1 && !(matriz[i] === 'x' || matriz[i] === 'o')){
+        square.style.backgroundImage = "url('../imgs/o.png')";
+        square.style.backgroundSize = "100% 100%"
+        player = 0;
+        matriz[i] = 'o';
+        jogadas++;
+        if(checaGanhador() === true){
+            matriz = ['', '', '', '', '', '', '', '', ''];
+            tituloVencedor.innerHTML = "O Circulo Ganhou!"
+            board.classList.add('blur');
+            jogadas = 0;
+        }
+        else if(checaGanhador() === false && jogadas === 9){
+            empate();
+            jogadas = 0;
+        }
+    }
+    else if(player == 0 && !(matriz[i] === 'x' || matriz[i] === 'o')){
+        square.style.backgroundImage = "url('../imgs/x.png')";
+        square.style.backgroundSize = "100% 100%"
+        player = 1;
+        matriz[i] = 'x';
+        jogadas++;
+        if(checaGanhador() === true){
+            matriz = ['', '', '', '', '', '', '', '', ''];
+            tituloVencedor.innerHTML = "O X Ganhou!"
+            board.classList.add('blur');
+            jogadas = 0;
+        }
+        else if(checaGanhador() === false && jogadas === 9){
+            empate();
+            jogadas = 0;
+        }
+    }
+
+}
+
+for (let i = 0; i < 9; i++) {
+    const square = document.createElement('div');
+    square.className = 'square';
+    square.id = `square${i}`;
+    square.addEventListener('click', () => colocarPeca(i, square))
+    board.appendChild(square);
+    quadrados.push(square);
+}
+
